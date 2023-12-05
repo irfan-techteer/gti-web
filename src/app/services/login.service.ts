c29973a81be551464d519066f8bb64aa97adf8f7
irfan-techteer <irfantechteer@gmail.com>
2023-12-05 16:03:46 +0530

    get best offer

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  // constructor(private router:Router) { }
+  // login(name:string, pass:string){
+  //   if (name === 'irfan' && pass === '1234'){
+  //     return 200;
+  //   }else{
+  //     return 403;
+  //   }
+  // }
+  // logout()
+  // {
+  //   this.router.navigate(['login'])
+
+  // }
+}
+
